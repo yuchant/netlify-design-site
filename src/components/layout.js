b71110147b36e7f6f4dac3a7d33a10a0c57dfe6f
Yuji Tomita <YTomita@rightpoint.com>
2020-10-28 14:38:06 -0700

    Remove content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,26 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import Logo from "./logo"
-import Navigation from "./navigation";
-
 import "../assets/scss/style.scss"
-import Footer from "./footer";
 
 const query = graphql`
-query LayoutQuery {
-  site {
-    siteMetadata {
-      siteTitle: title
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        siteTitle: title
+      }
     }
   }
-}
 `
 
-const Layout = ({children, className}) => {
-
+const Layout = ({ children, className }) => {
   const { site } = useStaticQuery(query)
-  const { siteTitle } = site.siteMetadata
 
   return (
     <div className="primary-container">
-      <Header>
-        <Logo title={siteTitle} />
-        <Navigation/>
-      </Header>
-      <main className={"container " + className}>
-        {children}
-      </main>
-      <Footer/>
+      <main className={"container " + className}>{children}</main>
     </div>
   )
 }
 
 export default Layout
-
